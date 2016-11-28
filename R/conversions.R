cd25fd011c438bb71681d282bf15172cb80c1f2f
timtrice <tim.trice@gmail.com>
2016-11-28 08:47:33 -0600

    Fit to under 80 characters in width

--- R/conversions.R
+++ R/conversions.R
@@ -1,12 +1,14 @@
 #' @title convert_lat_lon
-#' @description Converts lat, lon to negative if in southern, western hemisphere, respectively
+#' @description Converts lat, lon to negative if in southern, western 
+#'   hemisphere, respectively
 #' @param x integer
 #' @param y character
 #' @return integer positive or negative
+#' @export
 convert_lat_lon <- function(x, y) {
   
   if(!is.numeric(x)) {stop("x is not numeric!")}
-  if(!(y %in% c('N', 'S', 'E', 'W'))) { stop("y must be c('N', 'S', 'E', 'W')") }
+  if(!(y %in% c('N', 'S', 'E', 'W'))) { stop("y must be c('N','S','E','W')") }
   
   ifelse(y == 'S' | y == 'W', return(x * -1), return(x))
   
