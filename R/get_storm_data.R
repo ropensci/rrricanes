8386ee21734e53578993a3e84691d3c91073c9bd
timtrice <tim.trice@gmail.com>
2016-11-28 12:57:35 -0600

    Add .get_storm_content(), .get_products()

---
+++ R/get_storm_data.R
@@ -0,0 +1,53 @@
+#' @title .get_storm_content
+#' @description Extract content from a storm's archive page
+#' @param link of archive page
+#' @return page content of archive page
+.get_storm_content <- function(link) {
+  
+  # Get year
+  year <- .extract_year_archive_link(link)
+  
+  # There are different layouts for various years. Have to work through it...
+  if(year <= 2001) {
+    page <- xml2::read_html(link) %>% 
+      rvest::html_nodes('body') %>% 
+      rvest::html_nodes('table') %>% 
+      rvest::html_nodes('td') %>% 
+      rvest::html_children()
+    
+  } else {
+    page <- xml2::read_html(link) %>%
+      rvest::html_nodes('.center') %>%
+      rvest::html_nodes('.content') %>%
+      rvest::html_nodes('table') %>%
+      rvest::html_nodes('td') %>%
+      rvest::html_children()
+  }
+  
+  return(page)
+}
+
+#' @title .get_products
+#' @description Get list of all products from a storm's archive page
+#' @param link URL to storm's archive page.
+.get_products <- function(link) {
+  
+  # Get year
+  year <- .extract_year_archive_link(link)
+  
+  products <- .get_storm_content(link) %>% 
+    rvest::html_attr("href") %>% 
+    na.omit()
+  
+  nhc_url <- get_nhc_link(withTrailingSlash = FALSE)
+  
+  # Depending on the year these URL's are formatted various ways.
+  if(year == 1998) {
+    products <- stringr::str_c(nhc_url, '/archive/', year, '/', products)
+  } else {
+    products <- stringr::str_c(nhc_url, products)
+  }
+  
+  return(products)
+}
+
