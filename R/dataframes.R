c1ab263a87b7f766556bd5105b6590c6e13cfcbb
timtrice <tim.trice@gmail.com>
2016-11-28 12:53:51 -0600

    Add .drop_dataframes()

--- R/dataframes.R
+++ R/dataframes.R
@@ -14,3 +14,24 @@
                    "Link" = character())
   return(df)
 }
+
+#' @title .drop_dataframes
+#' @description Drop existing dataframes.
+.drop_dataframes <- function() {
+  
+  if(exists('df_advisories'))
+    rm(df_advisories, envir = .GlobalEnv)
+  
+  if(exists('df_forecasts'))
+    rm(df_forecasts, envir = .GlobalEnv)
+  
+  if(exists('df_forecast_winds'))
+    rm(df_forecast_winds, envir = .GlobalEnv)
+  
+  if(exists('df_winds'))
+    rm(df_winds, envir = .GlobalEnv)
+  
+  if(exists('df_seas'))
+    rm(df_seas, envir = .GlobalEnv)
+}
+
