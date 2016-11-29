8856799284aa74de957dd9e91f069a981ae49a34
timtrice <tim.trice@gmail.com>
2016-11-29 06:17:07 -0600

    Add base functionality for cyclone update prods

---
+++ R/updates.R
@@ -0,0 +1,67 @@
+#' @title get_updates
+#' @description Return dataframe of cyclone update data.
+#' \describe{
+#'   \item{Status}{Classification of storm, e.g., Tropical Storm, Hurricane, 
+#'     etc.}
+#'   \item{Name}{Name of storm}
+#'   \item{Adv}{Advisory Number}
+#'   \item{Date}{Date of advisory issuance}
+#'   \item{Contents}{Text content of product}
+#' }
+#' @param link URL to storm's archive page.
+#' @seealso \code{\link{get_storms}}, \code{\link{updates}}
+#' @export
+get_updates <- function(link) {
+  if(!.status(link))
+    stop(sprintf("Link unavailable. %d", l))
+  
+  products <- get_products(link)
+  
+  products.updates <- lapply(filter_updates(products), updates)
+  
+  updates <- data.table::rbindlist(products.updates)
+  
+  return(updates)
+}
+
+#' @title updates
+#' @description Parse cyclone update products
+#' @details Given a direct link to a cyclone update product, parse and return 
+#' dataframe of values.
+#' @param l Link to a storm's specific updates advisory product.
+#' @param display_link Display each link as being worked; default is TRUE.
+#' @return Dataframe
+#' @seealso \code{\link{get_updates}}
+#' @export
+updates <- function(l, display_link = TRUE) {
+  
+  if(!.status(l))
+    stop(sprintf("Link unavailable. %d", l))
+  
+  if(display_link)
+    message(sprintf("Working %s", l))
+  
+  contents <- l %>% 
+    xml2::read_html() %>% 
+    rvest::html_text()
+  
+  # Make sure this is a updates advisory product
+  if(!any(stringr::str_count(contents, c("MIATCUAT", "MIATCUEP"))))
+    stop(sprint("Invalid Public Advisory link. %s", l))
+  
+  df <- .create_df_public_advisories()
+  
+  status <- scrape_header(contents, ret = "status")
+  name <- scrape_header(contents, ret = "name")
+  adv <- scrape_header(contents, ret = "adv")
+  date <- scrape_header(contents, ret = "date")
+  
+  df <- df %>% 
+    tibble::add_row("Status" = status, 
+                    "Name" = name, 
+                    "Adv" = adv, 
+                    "Date" = date, 
+                    "Contents" = contents)
+  
+  return(df)
+}
