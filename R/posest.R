be21d46997f94d635a5605c8080c18fff64e760a
timtrice <tim.trice@gmail.com>
2016-11-29 05:58:57 -0600

    Relocated get_posest()

--- R/posest.R
+++ R/posest.R
@@ -1,3 +1,30 @@
+#' @title get_posest
+#' @description Return dataframe of position estimate data.
+#' \describe{
+#'   \item{Status}{Classification of storm, e.g., Tropical Storm, Hurricane, 
+#'     etc.}
+#'   \item{Name}{Name of storm}
+#'   \item{Adv}{Advisory Number}
+#'   \item{Date}{Date of advisory issuance}
+#'   \item{Contents}{Text content of product}
+#' }
+#' @param link URL to storm's archive page.
+#' @seealso \code{\link{get_storms}}, \code{\link{posest}}
+#' @export
+get_posest <- function(link) {
+  if(!.status(link))
+    stop(sprintf("Link unavailable. %d", l))
+  
+  products <- get_products(link)
+  
+  products.posest <- lapply(filter_discussions(products), posest)
+  
+  posest <- data.table::rbindlist(products.posest)
+  
+  return(posest)
+  
+}
+
 #' @title posest
 #' @description Extrapolate data from Position Estimate products. 
 #' @details Given a direct link to a position estimate product, parse and return 
