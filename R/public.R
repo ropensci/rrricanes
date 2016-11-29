40e0e769208b9b56bf0cc2272f91d14a2e03c8f7
timtrice <tim.trice@gmail.com>
2016-11-29 05:45:34 -0600

    Modified @seealso, added test to verify advisory product

--- R/public.R
+++ R/public.R
@@ -5,7 +5,7 @@
 #' @param l Link to a storm's specific public advisory product.
 #' @param display_link Display each link as being worked; default is TRUE.
 #' @return Dataframe
-#' @seealso \code{\link{get_public_advisories}}
+#' @seealso \code{\link{get_public}}
 #' @export
 public <- function(l, display_link = TRUE) {
 
@@ -18,6 +18,10 @@ public <- function(l, display_link = TRUE) {
   contents <- l %>% 
     xml2::read_html() %>% 
     rvest::html_text()
+  
+  # Make sure this is a public advisory product
+  if(!any(stringr::str_count(contents, c("MIATCPAT", "MIATCPEP"))))
+    stop(sprint("Invalid Public Advisory link. %s", l))
 
   df <- .create_df_public_advisories()
   
