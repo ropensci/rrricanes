8fc1e15b381e823c9b3e871c4bf1e0fe0c18efc1
timtrice <tim.trice@gmail.com>
2017-05-04 17:38:19 -0500

    Uncomment all but date tests

--- tests/testthat/test_public.R
+++ tests/testthat/test_public.R
@@ -13,25 +13,25 @@ test_that("Filter Public Advisories.", {
 
 al.1998.alex.products.public <- filter_public_advisories(al.1998.alex.products)
 
-# test_that("1998, Tropical Storm Alex, Advisory 1", {
-#   # Status
-#   expect_identical(public(al.1998.alex.products.public[1]) %>%
-#                      dplyr::select(Status) %>%
-#                      dplyr::first(), "TROPICAL DEPRESSION")
-#   # Name
-#   expect_identical(public(al.1998.alex.products.public[1]) %>%
-#                      dplyr::select(Name) %>%
-#                      dplyr::first(), "ONE")
-#   # Advisory number
-#   expect_equal(public(al.1998.alex.products.public[1]) %>%
-#                  dplyr::select(Adv) %>%
-#                  dplyr::first(), "1")
-#   # Date
-#   expect_equal(public(al.1998.alex.products.public[1]) %>%
-#                  dplyr::select(Date) %>%
-#                  dplyr::first(),
-#                as.POSIXct("1998-07-27 11:00:00", tz = "AST"))
-# })
+test_that("1998, Tropical Storm Alex, Advisory 1", {
+  # Status
+  expect_identical(public(al.1998.alex.products.public[1]) %>%
+                     dplyr::select(Status) %>%
+                     dplyr::first(), "TROPICAL DEPRESSION")
+  # Name
+  expect_identical(public(al.1998.alex.products.public[1]) %>%
+                     dplyr::select(Name) %>%
+                     dplyr::first(), "ONE")
+  # Advisory number
+  expect_equal(public(al.1998.alex.products.public[1]) %>%
+                 dplyr::select(Adv) %>%
+                 dplyr::first(), "1")
+  # # Date
+  # expect_equal(public(al.1998.alex.products.public[1]) %>%
+  #                dplyr::select(Date) %>%
+  #                dplyr::first(),
+  #              as.POSIXct("1998-07-27 11:00:00", tz = "AST"))
+})
 
 test_that("1998, Tropical Storm Alex, Advisory 26", {
   # Status
