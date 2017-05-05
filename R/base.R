5ab4165a529ae4f7f7fdeeff13308e67174fd163
timtrice <tim.trice@gmail.com>
2017-05-05 15:06:53 -0500

    Move functions from R/conversions.R to R/base.R

--- R/base.R
+++ R/base.R
@@ -23,6 +23,22 @@
 #' @name Hurricanes
 NULL
 
+#' @title convert_lat_lon
+#' @description Converts lat, lon to negative if in southern, western
+#'   hemisphere, respectively
+#' @param x integer
+#' @param y character
+#' @return integer positive or negative
+#' @export
+convert_lat_lon <- function(x, y) {
+
+    if(!is.numeric(x)) {stop("x is not numeric!")}
+    if(!(y %in% c('N', 'S', 'E', 'W'))) { stop("y must be c('N','S','E','W')") }
+
+    ifelse(y == 'S' | y == 'W', return(x * -1), return(x))
+
+}
+
 #' @title .extract_year_archive_link
 #' @description Extracts the year from the archive link. 
 #' @param link URL of archive page
@@ -45,6 +61,24 @@ get_nhc_link <- function(withTrailingSlash = TRUE) {
   if(withTrailingSlash)
     return('http://www.nhc.noaa.gov/')
   return('http://www.nhc.noaa.gov')
+}
+
+#' @title knots_to_mph
+#' @description convert knots (kt) to miles per hour (mph)
+#' @param x wind speed in knots
+#' @return x in mph
+#' @export
+knots_to_mph <- function(x) {
+    return(x * 1.15077945)
+}
+
+#' @title mb_to_in
+#' @description convert millibars (mb) to inches of mercury (in)
+#' @param x barometric pressure in mb
+#' @return x in in
+#' @export
+mb_to_in <- function(x) {
+    return(x * 0.029529983071)
 }
 
 #' @title month_str_to_num
