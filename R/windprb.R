38a7199655c788beade4f49a696a72691cc9d1fd
timtrice <tim.trice@gmail.com>
2017-05-05 10:28:55 -0500

    Correct wndprb to windprb to match product

--- R/windprb.R
+++ R/windprb.R
@@ -1,7 +1,7 @@
-#' @title get_wndprb
+#' @title get_windprb
 #' @description Return dataframe of wind probability data.
 #' \describe{
-#'   \item{Status}{Classification of storm, e.g., Tropical Storm, Hurricane, 
+#'   \item{Status}{Classification of storm, e.g., Tropical Storm, Hurricane,
 #'     etc.}
 #'   \item{Name}{Name of storm}
 #'   \item{Adv}{Advisory Number}
@@ -10,55 +10,55 @@
 #' }
 #' @param link URL to storm's archive page.
 #' @param msg Show link being worked. Default, FALSE.
-#' @seealso \code{\link{get_storms}}, \code{\link{wndprb}}
+#' @seealso \code{\link{get_storms}}, \code{\link{windprb}}
 #' @export
-get_wndprb <- function(link, msg = FALSE) {
+get_windprb <- function(link, msg = FALSE) {
 
   # Check status of link(s)
   valid.link <- sapply(link, .status)
   valid.link <- na.omit(valid.link)
   if(length(valid.link) == 0)
     stop("No valid links.")
-  
+
   products <- unlist(sapply(valid.link, get_products))
-  
-  products.wndprb <- lapply(filter_wind_probabilities(products), wndprb, msg = msg)
-  
-  wndprb <- data.table::rbindlist(products.wndprb)
-  
-  return(wndprb)
+
+  products.windprb <- lapply(filter_wind_probabilities(products), windprb, msg = msg)
+
+  windprb <- data.table::rbindlist(products.windprb)
+
+  return(windprb)
 }
 
-#' @title wndprb
+#' @title windprb
 #' @description Parse wind probability products
-#' @details Given a direct link to a wind probability product, parse and return 
+#' @details Given a direct link to a wind probability product, parse and return
 #' dataframe of values.
 #' @param link Link to a storm's specific wind probability product.
 #' @param msg Display each link as being worked; default is FALSE
 #' @return Dataframe
-#' @seealso \code{\link{get_wndprb}}
+#' @seealso \code{\link{get_windprb}}
 #' @export
-wndprb <- function(link, msg = FALSE) {
-  
+windprb <- function(link, msg = FALSE) {
+
   contents <- scrape_contents(link, msg = msg)
-  
-  # Make sure this is a wndprb advisory product
+
+  # Make sure this is a windprb advisory product
   if(!any(stringr::str_count(contents, c("MIAPWSAT", "MIAPWSEP"))))
     stop(sprint("Invalid Wind Probability link. %s", l))
-  
-  df <- .create_df_wndprb()
-  
+
+  df <- .create_df_windprb()
+
   status <- scrape_header(contents, ret = "status")
   name <- scrape_header(contents, ret = "name")
   adv <- scrape_header(contents, ret = "adv")
   date <- scrape_header(contents, ret = "date")
-  
-  df <- df %>% 
-    tibble::add_row("Status" = status, 
-                    "Name" = name, 
-                    "Adv" = adv, 
-                    "Date" = date, 
+
+  df <- df %>%
+    tibble::add_row("Status" = status,
+                    "Name" = name,
+                    "Adv" = adv,
+                    "Date" = date,
                     "Contents" = contents)
-  
+
   return(df)
 }
