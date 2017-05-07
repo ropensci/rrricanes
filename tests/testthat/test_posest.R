a4b11c8a2eb8338f4e0e1bc577fa6ff793d55fb1
timtrice <tim.trice@gmail.com>
2017-05-07 08:58:25 -0500

    Add test for get_posest()

--- tests/testthat/test_posest.R
+++ tests/testthat/test_posest.R
@@ -12,3 +12,21 @@ test_that("Dataframe Skeleton", {
     expect_identical(class(df$Date), c("POSIXct", "POSIXt"))
     expect_identical(class(df$Contents), "character")
 })
+
+## ---- Test get_posest() ------------------------------------------------------
+#' Test return of get_posest()
+test_that("Test get_posest()", {
+    url <- "http://www.nhc.noaa.gov/archive/2008/IKE.shtml?"
+    df <- get_posest(link = url)
+    expect_true(is.data.frame(df))
+    expect_true(is_tibble(df))
+    expect_identical(class(df$Status), "character")
+    expect_identical(class(df$Name), "character")
+    expect_identical(class(df$Adv), "character")
+    expect_identical(class(df$Date), c("POSIXct", "POSIXt"))
+    expect_identical(dim(df), as.integer(c(11, 5)))
+    ## ---- * Hurricane Ike, AL, 1998, position: 500 PM CDT --------------------
+    expect_identical(df$Status[1], "HURRICANE")
+    expect_identical(df$Name[1], "IKE")
+    expect_identical(df$Date[1], as.POSIXct("2008-09-12 22:00:00", tz = "UTC"))
+})
