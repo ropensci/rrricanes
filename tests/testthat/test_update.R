4d3f0231f9cbe6fa3b8abe5b53155d8004f9cabb
timtrice <tim.trice@gmail.com>
2017-05-07 10:29:22 -0500

    Add get_update() test

--- tests/testthat/test_update.R
+++ tests/testthat/test_update.R
@@ -12,3 +12,21 @@ test_that("Dataframe Skeleton", {
     expect_identical(class(df$Date), c("POSIXct", "POSIXt"))
     expect_identical(class(df$Contents), "character")
 })
+
+## ---- Test get_update() ------------------------------------------------------
+#' Test return of get_update()
+test_that("Test get_update()", {
+    url <- "http://www.nhc.noaa.gov/archive/2008/IKE.shtml?"
+    df <- get_update(link = url)
+    expect_true(is.data.frame(df))
+    expect_true(is_tibble(df))
+    expect_identical(class(df$Status), "character")
+    expect_identical(class(df$Name), "character")
+    expect_identical(class(df$Adv), "character")
+    expect_identical(class(df$Date), c("POSIXct", "POSIXt"))
+    expect_identical(dim(df), as.integer(c(3, 5)))
+    ## ---- * Hurricane Ike, AL, 1998, Sep 5 - update: 200 PM AST --------------
+    expect_identical(df$Status[1], "HURRICANE")
+    expect_identical(df$Name[1], "IKE")
+    expect_identical(df$Date[1], as.POSIXct("2008-09-05 18:00:00", tz = "UTC"))
+})
