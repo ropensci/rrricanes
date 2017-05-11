d450e753670205972552d8d6832a87041f936a6f
timtrice <tim.trice@gmail.com>
2017-05-11 18:31:59 -0500

    Add tests for get_prblty

--- tests/testthat/test_prblty.R
+++ tests/testthat/test_prblty.R
@@ -6,20 +6,13 @@ al1998 <- get_storms(year = 1998, basin = "AL") %>%
     dplyr::select(Link) %>%
     purrr::flatten_chr()
 
-## ---- Test prblty() ----------------------------------------------------------
-#' Test return of prblty()
-test_that("Test prblty()", {
-    ## ---- * Advisory #7 ------------------------------------------------------
-    url <- "http://www.nhc.noaa.gov/archive/1998/archive/prb/LAL0198.007"
-    df <- prblty(link = url)
-    expect_true(is.data.frame(df))
-    expect_true(is_tibble(df))
-    expect_identical(class(df$Status), "character")
-    expect_identical(class(df$Name), "character")
-    expect_identical(class(df$Adv), "character")
-    expect_identical(class(df$Date), c("POSIXct", "POSIXt"))
-    expect_identical(df$Status[1], "Tropical Storm")
-    expect_identical(df$Name[1], "Alex")
-    expect_identical(df$Adv[1], "7")
-    expect_identical(df$Date[1], as.POSIXct("1998-07-29 03:00:00", tz = "UTC"))
+df.al011998.prblty <- get_prblty(al1998[1])
+
+## ---- * Saved Data -----------------------------------------------------------
+load(system.file("extdata", "al011998.prblty.Rda", package = "Hurricanes"))
+
+## ---- Test prblty ------------------------------------------------------------
+#' Test return of prblty
+test_that("Test prblty", {
+    expect_identical(al011998.prblty, df.al011998.prblty)
 })
