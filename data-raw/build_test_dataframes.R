456b69b028fad1ad44f8eb674f21ca8a97a66305
timtrice <tim.trice@gmail.com>
2017-05-12 11:36:28 -0500

    Add tests for wndprb()

--- data-raw/build_test_dataframes.R
+++ data-raw/build_test_dataframes.R
@@ -13,6 +13,11 @@ ep1998 <- get_storms(year = 1998, basin = "EP") %>%
     dplyr::select(Link) %>%
     purrr::flatten_chr()
 
+#' Get storms for 2006, AL basin
+al2006 <- get_storms(year = 2006, basin = "AL") %>%
+    select(Link) %>%
+    flatten_chr()
+
 ## ---- fstadv -----------------------------------------------------------------
 
 ## ---- * 1998, AL, 01 ---------------------------------------------------------
@@ -37,3 +42,10 @@ save(ep021998.fstadv, file = "./inst/extdata/ep021998.fstadv.Rda", compression_l
 al011998.prblty <- get_prblty(al1998[1])
 save(al011998.prblty,
      file = "./inst/extdata/al011998.prblty.Rda", compression_level = 9)
+
+## ---- wndprb -----------------------------------------------------------------
+
+## ---- * 2006, AL, 01 ---------------------------------------------------------
+al012006.wndprb <- get_wndprb(al2006[1])
+save(al012006.wndprb,
+     file = "./inst/extdata/al012006.wndprb.Rda", compression_level = 9)
--- tests/testthat/test_wndprb.R
+++ tests/testthat/test_wndprb.R
@@ -1,14 +1,16 @@
 context("Wind Speed Probabilities (wndprb)")
 
-## ---- Dataframe Skeleton -----------------------------------------------------
-#' Test structure of dataframe skeleton
-test_that("Dataframe Skeleton", {
-    df <- create_df_wndprb()
-    expect_true(is.data.frame(df))
-    expect_true(is_tibble(df))
-    expect_identical(class(df$Status), "character")
-    expect_identical(class(df$Name), "character")
-    expect_identical(class(df$Adv), "character")
-    expect_identical(class(df$Date), c("POSIXct", "POSIXt"))
-    expect_identical(class(df$Contents), "character")
+## ---- Data -------------------------------------------------------------------
+## ---- * Current Data ---------------------------------------------------------
+al2006 <- get_storms(year = 2006, basin = "AL") %>% select(Link) %>% flatten_chr()
+
+df.al012006.wndprb <- get_wndprb(al2006[1])
+
+## ---- * Saved Data -----------------------------------------------------------
+load(system.file("extdata", "al012006.wndprb.Rda", package = "Hurricanes"))
+
+## ---- Test wndprb ------------------------------------------------------------
+#' Test return of wndprb
+test_that("Test wndprb", {
+    expect_identical(al012006.wndprb, df.al012006.wndprb)
 })
