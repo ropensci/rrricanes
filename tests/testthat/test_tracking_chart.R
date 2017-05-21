5ab9ae68f67b4e79f85e4fccd188805e0ba9ab57
timtrice <tim.trice@gmail.com>
2017-05-21 15:46:19 -0500

    Correct tracking chart error in test

--- tests/testthat/test_tracking_chart.R
+++ tests/testthat/test_tracking_chart.R
@@ -3,5 +3,5 @@ context("Test tracking chart")
 ## ---- tracking_chart() -------------------------------------------------------
 test_that("tracking_chart Errors", {
     expect_error(tracking_chart(res = "test"),
-                 "Chart resolution must be low, medium, or high")
+                 "Chart resolution must be 110, 50, 10")
 })
