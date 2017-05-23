d2dfc4b886a85542cc95589ab6b2685bf13fadb9
timtrice <tim.trice@gmail.com>
2017-05-23 17:06:47 -0500

    Transpose one-line wind speed probs
    If there is only one entry for wndprb tibble::as_data_frame moves
    matches to one column. Added quick fix to transpose in this instance.
    Added supporting test.
    
    All relevant tests pass.

--- tests/testthat/test_wndprb.R
+++ tests/testthat/test_wndprb.R
@@ -8,14 +8,17 @@ al2006 <- get_storms(year = 2006, basin = "AL") %>%
 
 df.al012006.wndprb <- get_wndprb(al2006[1])
 df.al032006.wndprb <- get_wndprb(al2006[3])
+df.al042006.wndprb <- get_wndprb(al2006[4])
 
 ## ---- * Saved Data -----------------------------------------------------------
 load(system.file("extdata", "al012006.wndprb.Rda", package = "Hurricanes"))
 load(system.file("extdata", "al032006.wndprb.Rda", package = "Hurricanes"))
+load(system.file("extdata", "al042006.wndprb.Rda", package = "Hurricanes"))
 
 ## ---- Test wndprb ------------------------------------------------------------
 #' Test return of wndprb
 test_that("Test wndprb", {
     expect_identical(al012006.wndprb, df.al012006.wndprb)
     expect_identical(al032006.wndprb, df.al032006.wndprb)
+    expect_identical(al042006.wndprb, df.al042006.wndprb)
 })
