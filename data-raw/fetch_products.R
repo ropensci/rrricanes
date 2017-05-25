956343f9e15a3f0404c037b42064d960bb7a68ea
timtrice <tim.trice@gmail.com>
2017-05-25 08:10:38 -0500

    Add ability to get specific storms

--- data-raw/fetch_products.R
+++ data-raw/fetch_products.R
@@ -35,12 +35,6 @@
 #' order. So, if the script is interruped and the last dataframe saved is
 #' AL2014_fstadv, the products variable must be modified with discus and fstadv
 #' removed. Otherwise, you're just being redundant.
-#'
-################################################################################
-#'
-#' DO NOT PUSH MODIFICATIONS OF THIS SCRIPT TO GITHUB.
-#'
-################################################################################
 
 ## ---- Libraries --------------------------------------------------------------
 library(assertr)
@@ -54,10 +48,19 @@ library(tibble)
 ## ---- Variables --------------------------------------------------------------
 # Save dataframe to global env?
 s <- TRUE
+# Where is data being saved?
 d <- "~/Projects/datasets/Hurricanes/"
+# Basins to retrieve
 basins <- c("AL", "EP")
+# Years to retrieve
 years <- 1998:as.numeric(strftime(Sys.Date(), "%Y"))
-products <- c("discus", "fstadv", "posest", "prblty", "public", "update", "wndprb")
+# All products to obtain
+products <- c("discus", "fstadv", "posest", "prblty", "public", "update",
+              "wndprb")
+# Get specific storm (by storm number for the year). Either handles all storms
+# with a NULL setting or one storm at a time (n <- 1 or n <- 2. n <- 1:2 will
+# not work).
+n <- 1
 
 ## ---- Functionality ----------------------------------------------------------
 # Walk through years
@@ -69,35 +72,69 @@ walk(years, .f = function(x) {
         # Walk through each product for current basin, current year
         pwalk(.l = list(year = x, basin = y, product = products),
               .f = function(basin, year, product) {
-                  # Load products for every storm of year, basin
-                  df <- paste("get", product, sep = "_") %>%
-                      invoke_map_df(.x = storms %>% .$Link)
-                  # Ignore if no product data available
-                  if (nrow(df) > 0) {
-                      # If product is fstadv, build a summary dataframe and
-                      # save/bring to global env
-                      if (product == "fstadv") {
-                          summary <- df %>%
-                              group_by(Key) %>%
-                              mutate(StartDate = min(Date),
-                                     EndDate = max(Date)) %>%
-                              select(Key, Name, Wind, StartDate, EndDate) %>%
-                              arrange(desc(Wind)) %>%
-                              filter(row_number() == 1) %>%
-                              arrange(Key)
-                          # Name of summary dataframe (i.e., AL1998)
-                          n <- sprintf("%s%s", basin, year)
-                          # Bring to global environment, if TRUE
-                          if (s) assign(n, summary, envir = .GlobalEnv)
-                          # Write summary dataframe
-                          write_csv(summary, path = sprintf("%s/%s.csv", d, n))
+                  # If requesting a specific storm, get data and save.
+                  if (!is.null(n)) {
+                      df <- sprintf("get_%s", product) %>%
+                          invoke_map_df(.x = storms %>% slice(n) %>% .$Link)
+                      if (nrow(df) > 0) {
+                          # Storm key. Strike probabilities products did not
+                          # have a Key value; only name. So, if the product is
+                          # `prblty`, build Key based off basin, n, and year. It
+                          # is possible this may be wrong (it shouldn't be). And
+                          # the only way to verify would be pulling another
+                          # product or pulling the storm summary which sort of
+                          # negates the purpose of what we're doing here. Any
+                          # discrepancies that are created because of this will
+                          # just have to be dealt with another way.
+                          if (product == "prblty") {
+                              k <- sprintf("%s%s%s", basin,
+                                           # Pad to two digits, if necessary
+                                           stringr::str_pad(n, 2, side = "left",
+                                                            pad = 0),
+                                           year)
+                          } else {
+                              # Otherwise, we can just select Key from the first row
+                              k <- df %>% slice(1) %>% .$Key
+                          }
+                          df.name <- sprintf("%s_%s", k, product)
+                          # Bring to global if s is TRUE
+                          if (s) assign(df.name, df, envir = .GlobalEnv)
+                          key_dir <- sprintf("%s/%s/%s/%s", d, basin, year, k)
+                          if (!dir.exists(key_dir)) dir.create(key_dir)
+                          write_csv(df, path = sprintf("%s/%s.csv", key_dir, df.name))
                       }
-                      # Name of product dataframe (i.e., AL1998_fstadv)
-                      n <- sprintf("%s%s_%s", basin, year, product)
-                      # Bring to global environment, if TRUE
-                      if (s) assign(n, df, envir = .GlobalEnv)
-                      # Save product dataframe
-                      write_csv(df, path = sprintf("%s/%s.csv", d, n))
+                  } else {
+                      # Otherwise, get all storms for current year
+                      df <- sprintf("get_%s", product) %>%
+                          invoke_map_df(.x = storms %>% .$Link)
+                      # Ignore if no product data available
+                      if (nrow(df) > 0) {
+                          # If product is fstadv, build a summary dataframe and
+                          # save/bring to global env
+                          if (product == "fstadv") {
+                              summary <- df %>%
+                                  group_by(Key) %>%
+                                  mutate(StartDate = min(Date),
+                                         EndDate = max(Date)) %>%
+                                  select(Key, Name, Wind, StartDate, EndDate) %>%
+                                  arrange(desc(Wind)) %>%
+                                  filter(row_number() == 1) %>%
+                                  arrange(Key)
+                              # Name of summary dataframe (i.e., AL1998)
+                              df.name <- sprintf("%s%s", basin, year)
+                              # Bring to global environment, if TRUE
+                              if (s) assign(df.name, summary, envir = .GlobalEnv)
+                              # Write summary dataframe
+                              write_csv(summary,
+                                        path = sprintf("%s/%s.csv", d, df.name))
+                          }
+                          # Name of product dataframe (i.e., AL1998_fstadv)
+                          df.name <- sprintf("%s%s_%s", basin, year, product)
+                          # Bring to global environment, if TRUE
+                          if (s) assign(df.name, df, envir = .GlobalEnv)
+                          # Save product dataframe
+                          write_csv(df, path = sprintf("%s/%s.csv", d, df.name))
+                      }
                   }
               })
         })
