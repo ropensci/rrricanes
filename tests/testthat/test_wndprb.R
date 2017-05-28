967d25ab52c9916b10449b799cd8a13c8f764c2c
timtrice <tim.trice@gmail.com>
2017-05-28 07:50:29 -0500

    Remove call to prblty stations datasets

--- tests/testthat/test_wndprb.R
+++ tests/testthat/test_wndprb.R
@@ -4,10 +4,6 @@ context("Wind Speed Probabilities (wndprb)")
 ## ---- * 2008, AL -------------------------------------------------------------
 df.al092008.wndprb <- al2008 %>% dplyr::slice(9) %>% .$Link %>% get_wndprb()
 load(system.file("extdata", "al092008.wndprb.Rda", package = "rrricanes"))
-## ---- AL probability stations ------------------------------------------------
-load(system.file("extdata", "al_prblty_stations.Rda", package = "rrricanes"))
-## ---- CP probability stations ------------------------------------------------
-load(system.file("extdata", "cp_prblty_stations.Rda", package = "rrricanes"))
 
 ## ---- Test get_wndprb() ------------------------------------------------------
 test_that("Test get_wndprb()", {
