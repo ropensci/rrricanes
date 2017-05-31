04ca670f83f3d0535c95646681cc0a7144529036
timtrice <tim.trice@gmail.com>
2017-05-30 21:46:55 -0500

    Drop GIS tests until funs streamlined

--- tests/testthat/test_gis.R
+++
@@ -1,6 +0,0 @@
-context("GIS data")
-
-## ---- Test fstadv_gis() ------------------------------------------------------
-test_that("Test fstadv_gis()", {
-    expect_error(fstadv_gis(key = "AL0198"), "Invalid key")
-})
