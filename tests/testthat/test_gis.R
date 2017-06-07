38917feb1e059d00e7e270ec9312d32fe2b0fb62
timtrice <tim.trice@gmail.com>
2017-06-07 10:59:24 -0500

    Correct typo

--- tests/testthat/test_gis.R
+++ tests/testthat/test_gis.R
@@ -2,6 +2,6 @@ context("Test GIS functions.")
 
 ## ---- gis_wsp ----------------------------------------------------------------
 test_that("NHC Link", {
-    expect_identcal(gis_wsp(datetime = "2016100606", res = 0.5),
+    expect_identical(gis_wsp(datetime = "2016100606", res = 0.5),
                     "http://www.nhc.noaa.gov/gis/forecast/archive/2016100606_wsp_120hrhalfDeg.zip")
 })
