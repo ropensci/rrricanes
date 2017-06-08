ffd91344575d559648a0c775f80aca011e46e915
timtrice <tim.trice@gmail.com>
2017-06-08 13:16:31 -0500

    Remove gis functions

--- tests/testthat/test_gis.R
+++
@@ -1,7 +0,0 @@
-context("Test GIS functions.")
-
-## ---- gis_wsp ----------------------------------------------------------------
-test_that("NHC Link", {
-    expect_identical(gis_wsp(datetime = "2016100606", res = 0.5),
-                    "http://www.nhc.noaa.gov/gis/forecast/archive/2016100606_wsp_120hrhalfDeg.zip")
-})
