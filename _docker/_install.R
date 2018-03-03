177bfdc599bcbf4834bda8c2402452fb74d7c447
Tim Trice <tim.trice@gmail.com>
2018-03-03 05:18:20 -0600

    Add MRAN repo

--- _docker/_install.R
+++ _docker/_install.R
@@ -1,3 +1,10 @@
+# set CRAN mirrors
+local({
+  r <- getOption("repos")
+	r["CRAN"] <- "https://mran.microsoft.com/snapshot/2017-07-16"
+  options(repos = r)
+})
+
 # rrricanes dependencies
 install.packages(c("broom", 
                    "crul", 
