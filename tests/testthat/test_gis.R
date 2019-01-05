590799364145d8a0bb6aecb87c98ae8184b78e5a
Tim Trice <tim.trice@gmail.com>
2019-01-05 08:56:34 -0600

    Change protocol. Again...

--- tests/testthat/test_gis.R
+++ tests/testthat/test_gis.R
@@ -4,7 +4,7 @@ context("Test GIS functions.")
 test_that("gis_advisory", {
   expect_identical(gis_advisory("AL092008", 32),
                    sprintf("%s%s%s",
-                           get_nhc_link(protocol = "http"),
+                           rrricanes:::get_nhc_link(),
                            "gis/forecast/archive/",
                            "al092008_5day_032.zip"))
 })
@@ -14,7 +14,7 @@ test_that("gis_advisory", {
 # test_that("gis_breakpoints", {
 #   expect_identical(gis_breakpoints(2017),
 #                    sprintf("%s%s%s",
-#                            get_nhc_link(),
+#                            rrricanes:::get_nhc_link(),
 #                            "gis/breakpoints/archive/",
 #                            "Breakpoints_2017.zip"))
 # })
@@ -23,7 +23,7 @@ test_that("gis_advisory", {
 test_that("gis_outlook", {
   expect_identical(gis_outlook(),
                    sprintf("%s%s",
-                           get_nhc_link(protocol = "http"),
+                           rrricanes:::get_nhc_link(),
                            "xgtwo/gtwo_shapefiles.zip"))
 })
 
@@ -34,7 +34,7 @@ test_that("gis_prob_storm_surge", {
                          products = list("psurge" = 0),
                          datetime = "2016090306"),
     sprintf("%s%s%s",
-            get_nhc_link(protocol = "http"),
+            rrricanes:::get_nhc_link(),
             "gis/storm_surge/",
             "al092016_psurge0_2016090306.zip"))
 })
@@ -43,7 +43,7 @@ test_that("gis_prob_storm_surge", {
 test_that("gis_windfield", {
   expect_identical(gis_windfield("AL142016", advisory = 30),
                    sprintf("%s%s%s",
-                           get_nhc_link(protocol = "http"),
+                           rrricanes:::get_nhc_link(),
                            "gis/forecast/archive/",
                            "al142016_fcst_030.zip"))
 })
@@ -52,7 +52,7 @@ test_that("gis_windfield", {
 test_that("NHC Link", {
   expect_identical(gis_wsp(datetime = "2016100606", res = 0.5),
                    sprintf("%s%s%s",
-                           get_nhc_link(protocol = "http"),
+                           rrricanes:::get_nhc_link(),
                            "gis/forecast/archive/",
                            "2016100606_wsp_120hrhalfDeg.zip"))
 })
